621e93907b099726ecc9b5b3cf2af2d165062a03
KnightMiner <knightminer4@gmail.com>
2020-07-05 15:39:40 -0500

    Implement melting recipes

--- src/main/java/slimeknights/tconstruct/library/recipe/RecipeTypes.java
+++ src/main/java/slimeknights/tconstruct/library/recipe/RecipeTypes.java
@@ -4,6 +4,7 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeType;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.recipe.material.MaterialRecipe;
+import slimeknights.tconstruct.library.recipe.melting.IMeltingRecipe;
 import slimeknights.tconstruct.library.recipe.partbuilder.PartRecipe;
 
 /**
@@ -12,6 +13,7 @@ import slimeknights.tconstruct.library.recipe.partbuilder.PartRecipe;
 public interface RecipeTypes {
   IRecipeType<PartRecipe> PART_BUILDER = register("part_builder");
   IRecipeType<MaterialRecipe> MATERIAL = register("material");
+  IRecipeType<IMeltingRecipe> MELTING = register("melting");
 
   /**
    * Registers a new recipe type, prefixing with the mod ID
